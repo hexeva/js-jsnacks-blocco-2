9257fdaac1989e3d072110e8d956fed2cffd792e
hexeva <patrick79.g@gmail.com>
2021-11-19 17:02:36 +0100

    push

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -5,10 +5,21 @@
 
 const numbers = [1,3,6,8,9,15,20,22,];
 
+let risultato = 0;
+
 for (let i = 0; i < numbers.length; i++){
 
     serialNumbers = numbers[i];
 
-    console.log(serialNumbers);
+    console.log(serialNumbers, 'indice', i);
+
+    if ( i % 2 != 0){
+        console.log(serialNumbers, 'indice', i)
+
+        risultato += serialNumbers;
+
+    }
 
 }
+
+console.log(risultato);
